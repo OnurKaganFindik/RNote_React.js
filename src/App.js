b2e61b8c64d4dda9481bbba8b7f61bc6a73d4698
OnurKaganFindik <45922689+OnurKaganFindik@users.noreply.github.com>
2021-10-15 10:22:44 +0300

    project files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, button } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <h1>RNote</h1>
+      <button className="btn btn-primary">Bir buton</button>
+      <Button variant="danger" >Bir buton</Button>
     </div>
   );
 }
